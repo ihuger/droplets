293946ea1ad48988a924bdc46910aed1ccf0031e
barroco <daixinghu@umeng.com>
2014-06-16 11:38:17 +0800

    update array

--- shell/array.sh
+++ shell/array.sh
@@ -1,7 +1,26 @@
-#!/usr/bin/env sh
+#!/bin/bash
+
+#array
+arr=(1 2 3 4 5)
+echo ${arr[0]} #1  (index by 0)
+echo ${arr[*]} #1 2 3 4 5 (all elements)
+echo ${arr[@]} #1 2 3 4 5 (all elements)
+echo ${#arr[*]} #5 (length)
+
+for num in ${arr[@]} 
+do
+    printf '%d\t' $num
+done
+
+echo
+
+#map
 declare -A phone
 phone=([jim]=135 [tom]=136 [lucy]=158)
-echo $phone
+echo '$phone no value : '$phone
+echo  '${phone[*]} is : '${phone[*]}
+echo  '${!phone[*]} is : '${!phone[*]}
+echo  '${#phone[*]} is : '${#phone[*]}
 
 for key in ${!phone[*]}
 do
